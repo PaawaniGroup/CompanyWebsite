e76bd34f1e6ef9a93be345bf1f07a291654530bc
Anshgupta0910 <anshikagupta7.paawanigroup@gmail.com>
2025-08-06 18:56:33 +0530

    Initital Setup Done

--- paawaniWebsite/src/App.jsx
+++ paawaniWebsite/src/App.jsx
@@ -1,19 +1,12 @@
 import React from 'react'
-import './App.css'
+// import Navbar from './components/navbar/navbar'
 
 function App() {
 
   return (
     <>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> */}
-      <h1>UNDER DEVELOPMENT</h1>
+
+    <h1>UNDER DEVELOPMENT</h1>
     </>
   )
 }
--- paawaniWebsite/vite.config.js
+++ paawaniWebsite/vite.config.js
@@ -1,3 +1,6 @@
+/* eslint-env node */
+
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -8,4 +11,9 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
