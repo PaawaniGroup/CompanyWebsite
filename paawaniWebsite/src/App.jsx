5a72494c380ce5e1c705c41fc70672ff2acb0062
Anshgupta0910 <anshikagupta7.paawanigroup@gmail.com>
2025-08-13 15:56:41 +0100

    Partnership Section added

--- paawaniWebsite/src/App.jsx
+++ paawaniWebsite/src/App.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import Navbar from './components/navbar/Navbar';
 import Hero from './components/hero/Hero';
 import Whyus from "./components/whyus/WhyUs"; 
-import OurPartners from "./components/ourPartners/OurPartners";
-
+import OurPartners from "./components/partners/OurPartners";
+import Partnership from "./components/partners/Partnership";
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
       <Hero />
       <Whyus />
       <OurPartners />
+      <Partnership />
       <div className="mx-auto h-screen flex flex-col items-center justify-center">
         <h1 className="text-5xl font-bold">UNDER DEVELOPMENT</h1>
       </div>
