d171165eba25bcca16bde0e9aa2ac97ca3d4a58e
Anshgupta0910 <anshikagupta7.paawanigroup@gmail.com>
2025-08-18 12:04:07 +0530

    Testimonial Section Added

--- paawaniWebsite/src/App.jsx
+++ paawaniWebsite/src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/hero/Hero';
 import Whyus from "./components/whyus/WhyUs"; 
 import OurPartners from "./components/partners/OurPartners";
 import Partnership from "./components/partners/Partnership";
+import Testimonials from "./components/testimonials/Testimonials";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Whyus />
       <OurPartners />
       <Partnership />
+      <Testimonials />
       <div className="mx-auto h-screen flex flex-col items-center justify-center">
         <h1 className="text-5xl font-bold">UNDER DEVELOPMENT</h1>
       </div>
