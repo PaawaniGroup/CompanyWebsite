f93c4620eff23f3eb17a7dac0c41598a0b28b34b
Anshgupta0910 <anshikagupta7.paawanigroup@gmail.com>
2025-08-28 18:12:58 +0530

    Toaster added

--- paawaniWebsite/src/main.jsx
+++ paawaniWebsite/src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
-// import { ModeToggle } from "@/components/ModeToggle";
+import { Toaster } from 'sonner';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
+    <Toaster richColors />
   </BrowserRouter>,
 )
