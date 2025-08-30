75d4e64e7e1f1a1b58c5a00e7bd8a7bfc3eae5c4
Anshgupta0910 <anshikagupta7.paawanigroup@gmail.com>
2025-08-30 17:05:24 +0530

    Name changed

--- paawaniWebsite/src/App.jsx
+++ paawaniWebsite/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom"
-import Navbar from '@/components/navbar/Navbar'
+import Navbar from '@/components/navbar/NavigationBar'
 import Home from '@/pages/Home'
 import About from '@/pages/About'
 import Business from '@/pages/business/OurBusinesses'
