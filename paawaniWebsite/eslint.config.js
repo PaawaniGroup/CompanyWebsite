3ce6a2ad167d926ad382a4c55dd28b09f7146a49
Anshgupta0910 <anshikagupta7.paawanigroup@gmail.com>
2025-08-30 16:02:49 +0530

    Linting Fixed

--- paawaniWebsite/eslint.config.js
+++ paawaniWebsite/eslint.config.js
@@ -5,12 +5,28 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
+    ignores: ["dist/", "build/"],
+  },
+  {
     files: ["**/*.{js,mjs,cjs,jsx}"],
     plugins: { js },
     extends: ["js/recommended"],
     languageOptions: { globals: globals.browser },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    }
   },
   pluginReact.configs.flat.recommended,
+
+  {
+    files: ["src/components/ui/*.{jsx,js}"],
+    rules: {
+      "react/prop-types": "off",
+    },
+  },
+
   // For config files that run in Node.js
   {
     files: ["vite.config.js", "eslint.config.js", "webpack.config.js", "*.config.js"],
--- paawaniWebsite/src/components/ui/.eslintrc.cjs
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-module.exports = {
-  extends: ["plugin:react/recommended"],
-  rules: {
-    // Disable prop-types validation for this folder
-    "react/prop-types": "off",
-  },
-};
